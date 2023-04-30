5c0e004c6f5be8fd58b3951fba648bc4e2ac7c69
Евгения Богданова <ev002@list.ru>
2023-04-30 23:25:34 +0700

    delete migrations, fix Dockerfile, fix load_data_to_postgres

--- infra/scripts/load_data_to_postgres.py
+++ infra/scripts/load_data_to_postgres.py
@@ -15,7 +15,7 @@ try:
         port=config['postgresql']['port'])
     cur = conn.cursor()
 
-    with open('../../data/ingredients.csv', 'r', encoding='utf-8') as f:
+    with open('../data/ingredients.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         for row in reader:
             cur.execute(f"INSERT INTO recipes_ingredient"
