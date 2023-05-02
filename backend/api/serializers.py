ed49755311a50a89e6302487a7d3e7a15c5cc107
Евгения Богданова <ev002@list.ru>
2023-05-03 03:18:28 +0700

    test run

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -115,8 +115,9 @@ class IngredientInRecipeCreateSerializer(serializers.ModelSerializer):
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
         # if isinstance(data, str) and data.startswith('data:image'):
-        if isinstance(data.get('image'), str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
+        if (isinstance(
+            data.get('image'), str)
+                and data.startswith('data:image')):
             ext = format.split('/')[-1]
             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
         return super().to_internal_value(data)
