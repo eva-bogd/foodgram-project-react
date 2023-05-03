93d1f431db32f8328fc466f2fb77dee7d6638baf
Евгения Богданова <ev002@list.ru>
2023-05-03 16:05:03 +0700

    fix bag with image

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,6 @@
-import base64
-
-from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserSerializer
+from drf_extra_fields.fields import Base64ImageField
 from recipes.models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
                             ShoppingCart, Subscribe, Tag)
 from rest_framework import serializers
@@ -110,15 +108,6 @@ class IngredientInRecipeCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = IngredientInRecipe
         fields = ('id', 'amount',)
-
-
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if (isinstance(data, str) and data.startswith('data:image')):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
 
 
 class RecipeGetSerializer(serializers.ModelSerializer):
